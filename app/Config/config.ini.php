0423020f75b9cf273367ee9830904eb508c57694
Jaideep Kumar <jaideepkumar1291@gmail.com>
2018-10-01 17:28:33 +0530

    orangescrum for debian 9 php 7

--- app/Config/config.ini.php
+++ app/Config/config.ini.php
@@ -1,8 +1,8 @@
 [EMAIL INFO]
-SMTP_UNAME = 
-SMTP_PWORD = 
-SMTP_HOST = 
-SMTP_PORT = 
+SMTP_UNAME = biswajit.andolasoft@gmail.com
+SMTP_PWORD = 9438565790
+SMTP_HOST = ssl://smtp.gmail.com
+SMTP_PORT = 587
 
 [PDF PATH]
 PDF_LIB_PATH = /usr/bin/wkhtmltopdf
