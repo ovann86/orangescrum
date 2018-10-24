1e5ecb0873102927391113af38842546e0cd6c70
Jyotiprakash <jyotiprakash.biswal@andolasoft.co.in>
2018-10-24 14:38:55 +0530

    Prefilled credentials removed

--- app/Config/config.ini.php
+++ app/Config/config.ini.php
@@ -1,8 +1,8 @@
 [EMAIL INFO]
-SMTP_UNAME = biswajit.andolasoft@gmail.com as@as.com a@as.com a@a.com 
-SMTP_PWORD = 9438565790 asd asd aa 
-SMTP_HOST = ssl://smtp.gmail.com ssl://52 ssl://25 ssl://25 
-SMTP_PORT = 587 25 25 25 
+SMTP_UNAME =  
+SMTP_PWORD =  
+SMTP_HOST =  
+SMTP_PORT = 
 
 [PDF PATH]
 PDF_LIB_PATH = /usr/bin/wkhtmltopdf
